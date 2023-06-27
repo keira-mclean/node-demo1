08c45a3b1533a59c14a4ce4539047483bf93da29
Keira-McLean_lmig <keira.mclean@libertymutual.com>
2023-06-27 12:29:54 -0400

    simple-statistic pkg

--- index.js
+++ index.js
@@ -15,3 +15,13 @@ console.log("Revised name: "+ name);
 name = lodash.upperCase(name);
 console.log("Upper Case name: "+ name);
 
+let ss = require("simple-statistics");
+let numbers = [62, 60, 53, 9, 11, 14, 15, 16, 17, 65, 88];
+
+let smallest = ss.min(numbers);
+let largest = ss.max(numbers);
+let sum = ss.sum(numbers);
+let average = ss.mean(numbers);
+
+console.log(smallest, largest, sum, average);
+
